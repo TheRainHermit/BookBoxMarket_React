114f72834fab4cbf72faa4b1f1e1d6b31d620146
TheRainHermit <miguel.fabra211294@gmail.com>
2025-09-03 13:07:23 -0500

    Despliegue 6: Intentando que pool funcione. Si no tocará pasarse a createClient.

--- src/db/pg/pgPool.js
+++ src/db/pg/pgPool.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.SUPABASE_URL, // Usa la URL de Supabase
+  connectionString: process.env.SUPABASE_DB_POOL_URL, // Usa la URL de Supabase
   ssl: { rejectUnauthorized: false }
 });
 
