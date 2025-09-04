faffe8b110e977252380b20a348c5e926469c434
TheRainHermit <miguel.fabra211294@gmail.com>
2025-09-04 12:38:40 -0500

    Despliegue 17: A ver si quitando api-docs deja.

--- api/api-docs-json.js
+++
@@ -1,9 +0,0 @@
-import specs from '../swagger.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Método no permitido' });
-  }
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(specs);
-}
